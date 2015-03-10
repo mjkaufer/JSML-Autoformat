c6fcab82f61e2ea5d3120fcb470b6db544cee746
Matthew Kaufer <mjkaufer@gmail.com>
2015-03-10 15:46:42 -0400

    Check if it's a JSML file

--- JSML-Autoformat.py
+++ JSML-Autoformat.py
@@ -2,16 +2,23 @@ import sublime, sublime_plugin
 
 class FormatArrayCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+		if not is_JSML(self.view):
+			return
+
 		print("Hello Array")
 		self.view.insert(edit, self.view.sel()[0].begin(), "[\n")
 		FormatChildCommand.run(self, edit, True)
 		self.view.insert(edit, self.view.sel()[0].begin(), "]")
+
 		# self.view.insert(edit, 0, "[")
 		# self.view.insert(edit, 0, "Hello, World!")
 		# Convert `[]`, when user types `[`, into [{t:"p",T:"dorem ipsum"}], formatted
 
 class FormatChildCommand(sublime_plugin.TextCommand):#todo - rather than always make autoformatted text, make sure the user is not putting the `{` after an `attributes` tag
 	def run(self, edit, nest=False):#if nest, we increase the tab count by 1 because the children are now nested in an array
+		if not is_JSML(self.view):
+			return
+
 		print("Hello Child")
 		tabs = ""
 
@@ -26,6 +33,14 @@ class FormatChildCommand(sublime_plugin.TextCommand):#todo - rather than always 
 		if nest:
 			self.view.insert(edit, self.view.sel()[0].begin(), "\n")
 
+
+def is_JSML(view):
+	filename = view.file_name();
+	
+	if not ".jsml" in filename.lower():
+		return False
+
+	return filename.lower().index(".jsml") == len(filename) - 5#ensure it's not part of the folder, etc., but rather the actual file extension
 
 		# self.view.insert(edit, 0, "{")
 		# self.view.insert(edit, 0, "Hello, World!")
